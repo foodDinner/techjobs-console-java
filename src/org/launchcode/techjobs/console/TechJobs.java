c75386d37636dfaa36c7890197b991b41135e9c7
foodDinner <specialdelivery@gmail.com>
2019-11-16 16:02:03 -0600

    Added initial printJobs stuff.

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -111,6 +112,12 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> testList : someJobs) {
+            System.out.println("*****");
+            for (Map.Entry<String, String> post : testList.entrySet()) {
+                System.out.println(post.getKey() + " : " + post.getValue());
+            }
+            System.out.println("*****" + "\n");
+        }
     }
 }
